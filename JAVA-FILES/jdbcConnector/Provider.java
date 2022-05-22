db8473661d576331fd95f26b17a7f0b040c0c316
Bhaktee Bhagwat Ugale <78139433+Bhaktee-Ugale@users.noreply.github.com>
2022-05-22 16:55:58 +0530

    Delete JAVA-FILES/jdbcConnector directory

--- JAVA-FILES/jdbcConnector/Provider.java
+++
@@ -1,8 +0,0 @@
-package com.jdbcConnector;
-
-public interface Provider {
-	String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-	String user = "root";
-	String pwd = "Bhaktee@2461";
-	String DB_URL = "jdbc:mysql://localhost:3306/Vaccination_Drive?useSSL=false";
-}
