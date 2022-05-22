197224501064e2e2a2f1f4ea12011230b228c3a6
Bhaktee Bhagwat Ugale <78139433+Bhaktee-Ugale@users.noreply.github.com>
2022-05-22 22:48:02 +0530

    Update VaccineRegistration.java
    Modification due to changes in Applicant.java class.

--- JAVA-FILES/com/vaccine_Registration/VaccineRegistration.java
+++ JAVA-FILES/com/vaccine_Registration/VaccineRegistration.java
@@ -1,6 +1,8 @@
 package com.vaccine_Registration;
 
-import java.sql.*;
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 import com.jdbcConnector.ConnectionProvider;
 
@@ -9,12 +11,23 @@ public class VaccineRegistration{
 	public static boolean register(Applicant a) {
 		// TODO Auto-generated method stub
 		int isDone=0;
-		if(a.getPreg().equals("Y") || a.getDia().equals("Y")) return false;
+		if(a.getPregnancy().equals("Y") || a.getDiabetes().equals("Y")) return false;
 		try {
 			Connection con = ConnectionProvider.getCon();
-			PreparedStatement ps = con.prepareStatement("insert into PersonalDet values(?,?,?,?,?,?,?,?,?,?,?,?,?)");
+			PreparedStatement ps = con.prepareStatement("insert into personal_det values(?,?,?,?,?,?,?,?,?,?,?,?,?)");
 			ps.setString(1, a.getAdhno());
-			
+			ps.setString(2, a.getName());
+			ps.setString(3, a.getGender());
+			ps.setDate(4, null); //convert string to date
+			ps.setString(5, a.getCity());
+			ps.setString(6, a.getState());
+			ps.setString(7, a.getPncd());
+			ps.setString(8, a.getMobno());
+			ps.setString(9, a.getEmailId());
+			ps.setString(10, a.getPregnancy());
+			ps.setString(11, a.getDiabetes());
+			ps.setString(12, a.getOther());
+			ps.setString(13, a.getPwd());
 			isDone = ps.executeUpdate();
 			if(isDone>0) return true;
 			con.close();
