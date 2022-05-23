9222123cdd4fcf2c9c04c3f205762693ee7bf81d
Bhaktee Bhagwat Ugale <78139433+Bhaktee-Ugale@users.noreply.github.com>
2022-05-23 16:16:07 +0530

    Update VaccineBooking.java

--- JAVA-FILES/com/vaccine_booking/VaccineBooking.java
+++ JAVA-FILES/com/vaccine_booking/VaccineBooking.java
@@ -3,37 +3,49 @@ package com.vaccine_booking;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import com.jdbcConnector.ConnectionProvider;
 
 public class VaccineBooking {
 	
-	private static boolean isBooked(String adhno) {
+	public static boolean isBooked(String adhno) {
 		boolean is=false;
 		try {
 			Connection con = ConnectionProvider.getCon();
 			PreparedStatement ps = con.prepareStatement("select isBooked(?);");
 			ps.setString(1, adhno);
 			ResultSet rs=ps.executeQuery();
-			is=rs.next();
-			con.close();
+			while(rs.next()) {
+				if(rs.getInt(1)==1) is=true;
+			}
 			
+			con.close();
 		}
-		catch(Exception e) {}
+		catch(SQLException e) {}
 		return is;
 	}
+	
+	public static boolean bookVaccine(String adhno) {
+		boolean status = isBooked(adhno);
+		if(status) return false;
+		try {
+			Connection con = ConnectionProvider.getCon();
+			PreparedStatement p = con.prepareStatement("select MobNo from personal_det where Adhno=?");
+			p.setString(1, adhno);
+			ResultSet rs=p.executeQuery();
+			int bno;
+			String ans;
+			while(rs.next()) {
+				ans = rs.getString(1);
+				
+			}
+			p = con.prepareStatement("");
+			p.execute();
+			con.close();
+		}
+		catch(SQLException e) {}
+		
+		return status;
+	}
 }
-/*
- * create function isBooked(adhno String)
- * returns int
- * begin
- * declare is_b int default 0;
- * declare bno int default -1;
- * set bno = select bno from PersonalDet where adhno=adhno;
- * if bno!=-1 then
- * set is_b=1;
- * end if;
- * return is_b;
- * end//
- *
- */
