53e7a42391b68b610ebf45bc1a365ae514379886
Bhaktee Bhagwat Ugale <78139433+Bhaktee-Ugale@users.noreply.github.com>
2022-05-23 09:24:23 +0530

    Update Provider.java

--- JAVA-FILES/com/jdbcConnector/Provider.java
+++ JAVA-FILES/com/jdbcConnector/Provider.java
@@ -4,5 +4,5 @@ public interface Provider {
 	String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 	String user = "root";
 	String pwd = "Bhaktee@2461";
-	String DB_URL = "jdbc:mysql://localhost:3306/Vaccination_Drive?useSSL=false";
+	String DB_URL = "jdbc:mysql://localhost:3306/mini_project_db?useSSL=false";
 }
