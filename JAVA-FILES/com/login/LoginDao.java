4233a1806444f56e9be65aafc7471e5b40f0c824
Bhaktee Bhagwat Ugale <78139433+Bhaktee-Ugale@users.noreply.github.com>
2022-05-23 16:13:00 +0530

    Update LoginDao.java
    Some additional changes due to addition of columns to tables

--- JAVA-FILES/com/login/LoginDao.java
+++ JAVA-FILES/com/login/LoginDao.java
@@ -8,11 +8,14 @@ public class LoginDao {
 		boolean status = false;
 		try {
 			Connection con = ConnectionProvider.getCon();
-			PreparedStatement ps = con.prepareStatement("select * from PersonalDet where adhno=? and pwd=?");
+			PreparedStatement ps = con.prepareStatement("select Aadhar_No, pwd from personal_det where Aadhar_No=? and pwd=?");
 			ps.setString(1, al.getAdhno());
 			ps.setString(2, al.getPwd());
-			ResultSet rs=ps.executeQuery();  
-			status=rs.next();
+			ResultSet rs=ps.executeQuery();
+			while(rs.next()) {
+				if(al.getAdhno().equals(rs.getString(1)) && al.getPwd().equals(rs.getString(2))) status=true;
+			}
+			//con.close();
 		}
 		catch(SQLException e) {
 			e.printStackTrace();
