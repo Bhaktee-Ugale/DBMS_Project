161f8cd0cb40faa7f030fcf3e044784c8f65c744
Bhaktee Bhagwat Ugale <78139433+Bhaktee-Ugale@users.noreply.github.com>
2022-05-23 16:14:19 +0530

    Update VaccineRegistration.java
    Additional changes due to addition of columns to table personal_det in database

--- JAVA-FILES/com/vaccine_Registration/VaccineRegistration.java
+++ JAVA-FILES/com/vaccine_Registration/VaccineRegistration.java
@@ -3,6 +3,7 @@ package com.vaccine_Registration;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Date;
 
 import com.jdbcConnector.ConnectionProvider;
 
@@ -14,14 +15,16 @@ public class VaccineRegistration{
 		if(a.getPregnancy().equals("Y") || a.getDiabetes().equals("Y")) return false;
 		try {
 			Connection con = ConnectionProvider.getCon();
-			PreparedStatement ps = con.prepareStatement("insert into personal_det values(?,?,?,?,?,?,?,?,?,?,?,?,?)");
-			ps.setString(1, a.getAdhno());
-			ps.setString(2, a.getName());
-			ps.setString(3, a.getGender());
-			ps.setDate(4, null); //convert string to date
-			ps.setString(5, a.getCity());
+			PreparedStatement ps = con.prepareStatement("insert into personal_det(Aadhar_No, Name, DOB, Gender, City, State, Pincode, MobNo, emailId, Pregnancy, Diabetes, Other_Ill, pwd) values(?,?,?,?,?,?,?,?,?,?,?,?,?)");
+			ps.setString(1, a.getAdhno()); //aadhar number
+			ps.setString(2, a.getName());  //name
+			ps.setString(4, a.getGender()); //gender
+			java.sql.Date n_dob=Date.valueOf(a.getDob());
+			ps.setDate(3, n_dob); //convert string to date
+			ps.setString(5, a.getCity()); //city
 			ps.setString(6, a.getState());
-			ps.setString(7, a.getPncd());
+			int n_pncd = Integer.parseInt(a.getPncd());
+			ps.setInt(7, n_pncd);  //pincode
 			ps.setString(8, a.getMobno());
 			ps.setString(9, a.getEmailId());
 			ps.setString(10, a.getPregnancy());
